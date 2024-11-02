d8d717d6fb4c98bda0cf218bc41f3f8301726a4c
Dương Thái Bảo <me@dtbao.io.vn>
2024-11-02 17:42:17 +0700

    add: entity User

---
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/entites/User.java
@@ -0,0 +1,15 @@
+package com.fit.iuh.entites;
+
+import java.util.Date;
+
+public class User {
+	private int userId;
+	
+	private String name;
+	
+	private int role;
+	
+	private String accountState, email, title, passwordHash, profilePicture, bio;
+	
+	private Date createdAt, updatedAt;
+}
