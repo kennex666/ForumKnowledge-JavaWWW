56de065fafe68ad9b9cb600c381f78a15c70f7ba
LeNguyenDuyKhang <duykhang0175@gmail.com>
2024-11-19 19:37:27 +0700

    Add: repository - service (interface, impl) of Comment, Following

---
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/repositories/CommentRepository.java
@@ -0,0 +1,25 @@
+package com.fit.iuh.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.fit.iuh.entites.Comment;
+
+@RepositoryRestResource
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+	@Query("SELECT c FROM Comment c WHERE c.user.userId  = :userId")
+	List<Comment> findAllByUserId(@Param("userId") int userId);
+	
+    @Query("SELECT c FROM Comment c WHERE c.post.postId = :postId")
+    List<Comment> findAllByPostId(@Param("postId") int postId);
+    
+    @Query("DELETE FROM Comment c WHERE c.commentId = :commentId")
+	public Boolean delete(@Param("commentId") int commentId);
+    
+    
+
+}
