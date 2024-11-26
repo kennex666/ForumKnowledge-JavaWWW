63ffa9192bfc6df6c42d4f572ed428798e4b6d10
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-27 01:23:45 +0900

    update: AdminPostController class

--- Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/AdminPostController.java
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/AdminPostController.java
@@ -3,23 +3,16 @@ package com.fit.iuh.controllers;
 import com.fit.iuh.entites.Post;
 import com.fit.iuh.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
-//@RestController
 @Controller
 @RequestMapping("/admin/posts")
-public class PostController {
+public class AdminPostController {
 
     @Autowired
     private PostService postService;
@@ -30,4 +23,5 @@ public class PostController {
         model.addAttribute("posts", posts);
         return "posts/post-list";
     }
+
 }
