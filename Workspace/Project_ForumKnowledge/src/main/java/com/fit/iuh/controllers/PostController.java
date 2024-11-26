d0666089a311a3fcd45b7abdd78af2e7874ddd55
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-27 01:14:14 +0900

    add: admin controller

--- Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/PostController.java
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/PostController.java
@@ -5,7 +5,10 @@ import com.fit.iuh.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +16,18 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+//@RestController
+@Controller
+@RequestMapping("/admin/posts")
 public class PostController {
+
     @Autowired
     private PostService postService;
 
-    @GetMapping("/search")
-    public ResponseEntity<Map<String, Object>> search(@RequestParam("keyword") String keyword) {
-        Map<String, Object> response = new LinkedHashMap<>();
-
-        response.put("status", HttpStatus.OK.value());
-
-        response.put("data", postService.search(keyword));
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Post> posts = postService.findAll();
+        model.addAttribute("posts", posts);
+        return "posts/post-list";
     }
 }
