5f87a79669c2e14a678ad3479efd0bc23c7d252f
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-29 06:50:40 +0900

    update: user-profile-main view

--- Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/UserController.java
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/UserController.java
@@ -1,11 +1,25 @@
 package com.fit.iuh.controllers;
 
+import com.fit.iuh.entites.User;
 import com.fit.iuh.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/user")
+    public String getUser(Model model, @RequestParam("id") int id) {
+        User user = userService.findUserById(id);
+        model.addAttribute("user", user);
+        return "views_user/user-profile-main";
+    }
+
 }
