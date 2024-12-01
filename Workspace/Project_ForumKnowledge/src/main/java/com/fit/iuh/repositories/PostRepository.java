734672a8538452f019d404234a71e3537ff0d67e
Dương Thái Bảo <me@dtbao.io.vn>
2024-12-01 16:39:31 +0700

    fix: post repository - add option for vote

--- Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/repositories/PostRepository.java
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/repositories/PostRepository.java
@@ -45,4 +45,8 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
 	@Query("SELECT p FROM Post p")
 	public Page<Post> findAllPost(Pageable pageable);
+
+	@Query("SELECT p FROM Post p JOIN p.author.followers f WHERE f.followed.userId = :userId")
+	public Page<Post> findForFollowing(Pageable pageable, int userId);
+
 }
