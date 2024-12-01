f6b17d832b3e9ccb44007d85c74680085d90b6b5
Dương Thái Bảo <me@dtbao.io.vn>
2024-12-01 23:43:23 +0700

    feat: search by author, topic

--- Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/SearchController.java
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/SearchController.java
@@ -1,12 +1,15 @@
 package com.fit.iuh.controllers;
 
+import com.fit.iuh.entites.User;
 import com.fit.iuh.services.PostService;
+import com.fit.iuh.utilities.SpringContext;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/search")
@@ -18,14 +21,24 @@ public class SearchController {
     }
 
     @RequestMapping
-    public String search(Model model, @Param("q") String q) {
+    public String search(Model model, @Param("q") String q, @RequestParam(defaultValue = "keyword", required = false) String tab, @RequestParam(defaultValue = "0") int page) {
         Pageable pageable = PageRequest.of(0, 5);  // 10 bài viết mỗi trang
         model.addAttribute("rcmPost", postService.findForHome(pageable));
 
         model.addAttribute("title", "Tìm kiếm | iDev4rum");
         model.addAttribute("q", q);
 
-        model.addAttribute("searchPosts", postService.searchKeyword(pageable, q));
+
+        String tabStr = tab == null ? "keyword" : tab.isBlank() ? "keyword" : tab;
+
+        if (tabStr.equals("author")) {
+            model.addAttribute("searchPosts", postService.getPostByAuthor(pageable, q));
+        } else if (tabStr.equals("tag")) {
+            model.addAttribute("searchPosts", postService.getPostByTopic(pageable, q));
+        } else {
+            model.addAttribute("searchPosts", postService.searchKeyword(pageable, q));
+        }
+
         return "views_user/search";
     }
 }
