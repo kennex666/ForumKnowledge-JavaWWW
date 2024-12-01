a93739e8185247983a3c4c8dc534c7ade5f745f4
Dương Thái Bảo <me@dtbao.io.vn>
2024-12-01 16:39:02 +0700

    fix: view user

--- Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/UserController.java
+++ Workspace/Project_ForumKnowledge/src/main/java/com/fit/iuh/controllers/UserController.java
@@ -6,34 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/author")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user-profile")
-    public String getUser(Model model, @RequestParam("id") int id) {
+    @GetMapping("/{id}")
+    public String getUser(Model model, @PathVariable("id") int id, @RequestParam(value = "tab", required = false) String tab) {
         User user = userService.findUserById(id);
         model.addAttribute("user", user);
+        model.addAttribute("listPost", user.getPosts());
+        if (tab != null) {
+            if (tab.equals("about")) {
+                return "views_user/user-profile-about";
+            } else {
+                return "views_user/user-profile-main";
+            }
+        }
         return "views_user/user-profile-main";
     }
-
-    @GetMapping("/user-posts")
-    public String getUserPosts(Model model, @RequestParam("id") int id) {
-        User user = userService.findUserById(id);
-        model.addAttribute("user", user);
-        return "views_user/user-profile-posts";
-    }
-
-    @GetMapping("/user-about")
-    public String getUserAbout(Model model, @RequestParam("id") int id) {
-        User user = userService.findUserById(id);
-        model.addAttribute("user", user);
-        return "views_user/user-profile-about";
-    }
-
 }
